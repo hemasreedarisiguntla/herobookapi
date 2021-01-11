97889e8ab5a4e9b3405eee0500953b964170c014
Hemasree Darisiguntla <sreed2503@gmail.com>
2021-01-11 18:47:06 -0500

    Initial commit

---
+++ src/test/java/com/cognizant/herobookapi/herobookapi/HerobookapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.herobookapi.herobookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerobookapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
