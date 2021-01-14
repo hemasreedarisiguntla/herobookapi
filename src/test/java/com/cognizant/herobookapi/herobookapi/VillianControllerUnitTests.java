e5923f5e3f4cb136368b0953d1aa67acab74dc53
Hemasree Darisiguntla <sreed2503@gmail.com>
2021-01-14 18:44:30 -0500

    Implemented getAllvillians unit test to test empty

---
+++ src/test/java/com/cognizant/herobookapi/herobookapi/VillianControllerUnitTests.java
@@ -0,0 +1,42 @@
+package com.cognizant.herobookapi.herobookapi;
+
+import com.cognizant.herobookapi.herobookapi.controllers.VillianController;
+import com.cognizant.herobookapi.herobookapi.service.HeroService;
+import com.cognizant.herobookapi.herobookapi.service.VillianService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(VillianController.class)
+public class VillianControllerUnitTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    VillianService villianService;
+
+    ArrayList<String> villainsList;
+
+    @Test
+    public void getAllVillians_size_zero() throws Exception {
+
+        villainsList = new ArrayList<>();
+        when(villianService.getAllVillians()).thenReturn(villainsList);
+        mockMvc.perform(get("/api/villians"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+}
